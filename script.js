648360d52f77ae9b83c9f81d5befa7e5344f530e
SwathiPriya37 <rswathipriya3@gmail.com>
2025-05-27 20:12:16 +0530

    added email function properties

---
+++ script.js
@@ -0,0 +1,17 @@
+var emailField = document.getElementById("email-field");
+var emailLabel = document.querySelector("email-label");
+var emailError = document.getElementById("email-error");
+
+function validateEmail() {
+    emailLabel.style.bottom = "45px";
+
+    if (!emailField.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+        emailError.innerHTML = "Please enter a valid email address";
+        emailField.style.borderColor = "red";
+        emailError.style.top= '120%'
+        return false;
+    }
+    emailError.innerHTML = "";
+    emailField.style.borderColor = "green";
+    return true;
+}
