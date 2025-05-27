daf30b5507063360b5b03baf2750945ccd7689e9
SwathiPriya37 <rswathipriya3@gmail.com>
2025-05-27 20:23:22 +0530

    modified file

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var emailField = document.getElementById("email-field");
-var emailLabel = document.querySelector("email-label");
-var emailError = document.getElementById("email-error");
+  var emailField = document.getElementById("email-field");
+        var emailLabel = document.getElementById("email-label");
+        var emailError = document.getElementById("email-error");
 
-function validateEmail() {
-    emailLabel.style.bottom = "45px";
+        function validateEmail() {
+            emailLabel.style.bottom = "45px";
 
-    if (!emailField.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
-        emailError.innerHTML = "Please enter a valid email address";
-        emailField.style.borderColor = "red";
-        emailError.style.top= '120%'
-        return false;
-    }
-    emailError.innerHTML = "";
-    emailField.style.borderColor = "green";
-    return true;
-}
+            if (!emailField.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+                emailError.innerHTML = "Please enter a valid email address";
+                emailField.style.borderColor = "red";
+                emailError.style.top = '120%';
+                return false;
+            }
+            
+            emailError.innerHTML = "";
+            emailError.style.top = '100px';
+            emailField.style.borderColor = "green";
+            return true;
+        }
